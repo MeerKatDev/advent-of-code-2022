aac1818fd70df3f264aab8f3dc329cadcff5a842
Luca Campobasso <lcampobasso@gmail.com>
2022-12-06 11:23:34 +0100

    day6: part two

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let path = Path::new("input.txt");
     let text = from_file(path);
 
-    println!("marker: {}", get_marker(text.as_str()));
+    println!("marker: {}", get_marker(text.as_str(), 14));
 }
 
 fn from_file(path: &Path) -> String {
@@ -16,12 +16,12 @@ fn from_file(path: &Path) -> String {
     text
 }
 
-fn get_marker(s: &str) -> usize {
+fn get_marker(s: &str, num: usize) -> usize {
     let mut tmp: &str;
     let mut idx_fwd: usize = 0;
 
-    for idx in 0..(s.len() - 4) {
-        idx_fwd = idx + 4;
+    for idx in 0..(s.len() - num) {
+        idx_fwd = idx + num;
         tmp = &s[idx..idx_fwd];
         if is_uniq(tmp) {
             break;
@@ -43,28 +43,57 @@ fn is_uniq(s: &str) -> bool {
 mod tests {
     use super::*;
 
+    // packet marker
+
     #[test]
     fn demo_zero() {
-        assert_eq!(get_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(get_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
     }
 
     #[test]
     fn demo_one() {
-        assert_eq!(get_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(get_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
     }
 
     #[test]
     fn demo_two() {
-        assert_eq!(get_marker("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(get_marker("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
     }
 
     #[test]
     fn demo_three() {
-        assert_eq!(get_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(get_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
     }
 
     #[test]
     fn demo_four() {
-        assert_eq!(get_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(get_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+    }
+
+    // message marker
+    
+    #[test]
+    fn demo_zero_p2() {
+        assert_eq!(get_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+    }
+
+    #[test]
+    fn demo_one_p2() {
+        assert_eq!(get_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+    }
+
+    #[test]
+    fn demo_two_p2() {
+        assert_eq!(get_marker("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+    }
+
+    #[test]
+    fn demo_three_p2() {
+        assert_eq!(get_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+    }
+
+    #[test]
+    fn demo_four_p2() {
+        assert_eq!(get_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
     }
 }
