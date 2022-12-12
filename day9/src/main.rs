c8af05204969fb96c93d1f8c39a69495c8520735
Luca Campobasso <lcampobasso@gmail.com>
2022-12-12 10:40:19 +0100

    day2: almost correct

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -5,105 +5,143 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 const CENTER: usize = 400;
+const KNOTS_NUM: usize = 10;
+
+type Coords = Vec<(usize, usize)>;
 
 struct Grid {
-    head_pos_x: usize,
-    head_pos_y: usize,
-    tail_pos_x: usize,
-    tail_pos_y: usize,
-    visited: Vec<(usize, usize)>,
+    knots_positions: [(usize, usize);KNOTS_NUM],
+    visited: Coords,
 }
 
 impl Grid {
     pub fn new() -> Self {
         Self {
-            head_pos_x: CENTER,
-            head_pos_y: CENTER,
-            tail_pos_x: CENTER,
-            tail_pos_y: CENTER,
+            knots_positions: [(CENTER,CENTER);KNOTS_NUM],
             visited: vec![(CENTER, CENTER)],
         }
     }
 
     pub fn move_right_by(&mut self, n: usize, acc: &mut usize) {
         for _step in 0..n {
-            self.head_pos_x += 1;
+            self.knots_positions[0].0 += 1;
 
-            if self.too_far() {
-                self.tail_pos_x += 1;
-                self.tail_recover_vertical();
-                self.register_position(acc);
+            for idx in 1..KNOTS_NUM {
+                if self.too_far_from_previous(idx) {
+                    self.knots_positions[idx].0 += 1;
+                    self.tail_recover_vertical(idx);
+                    if idx == (KNOTS_NUM - 1) { self.register_position(acc); }
+                }
             }
         }
     }
 
     pub fn move_up_by(&mut self, n: usize, acc: &mut usize) {
         for _step in 0..n {
-            self.head_pos_y += 1;
+            self.knots_positions[0].1 += 1;
 
-            if self.too_far() {
-                self.tail_pos_y += 1;
-                self.tail_recover_horizontal();
-                self.register_position(acc);
+            for idx in 1..KNOTS_NUM {
+                if self.too_far_from_previous(idx) {
+                    self.knots_positions[idx].1 += 1;
+                    self.tail_recover_horizontal(idx);
+                    if idx == (KNOTS_NUM - 1) { self.register_position(acc); }
+                }
             }
         }
     }
 
     pub fn move_left_by(&mut self, n: usize, acc: &mut usize) {
         for _step in 0..n {
-            self.head_pos_x -= 1;
+            self.knots_positions[0].0 -= 1;
 
-            if self.too_far() {
-                self.tail_pos_x -= 1;
-                self.tail_recover_vertical();
-                self.register_position(acc);
+            for idx in 1..KNOTS_NUM {
+                if self.too_far_from_previous(idx) {
+                    self.knots_positions[idx].0 -= 1;
+                    self.tail_recover_vertical(idx);
+                    if idx == (KNOTS_NUM - 1) { self.register_position(acc); }
+                }
             }
         }
     }
 
     pub fn move_down_by(&mut self, n: usize, acc: &mut usize) {
         for _step in 0..n {
-            self.head_pos_y -= 1;
+            self.knots_positions[0].1 -= 1;
 
-            if self.too_far() {
-                self.tail_pos_y -= 1;
-                self.tail_recover_horizontal();
-                self.register_position(acc);
+            for idx in 1..KNOTS_NUM {
+                if self.too_far_from_previous(idx) {
+                    self.knots_positions[idx].1 -= 1;
+                    self.tail_recover_horizontal(idx);
+                    if idx == (KNOTS_NUM - 1) { self.register_position(acc); }
+                }
             }
         }
     }
 
-    // debug
-    // pub fn print_coords(&self) {
-    //     println!("H({},{}), T({},{})", self.head_pos_x, self.head_pos_y, self.tail_pos_x, self.tail_pos_y);
-    // }
-
     fn register_position(&mut self, acc: &mut usize) {
-        if !self.visited.contains(&(self.tail_pos_x, self.tail_pos_y)) {
+        if !self.visited.contains(&self.knots_positions[KNOTS_NUM - 1]) {
             *acc += 1;
-            self.visited.push((self.tail_pos_x, self.tail_pos_y))
+            println!("cx: ({}, {})", self.knots_positions[KNOTS_NUM - 1].0, self.knots_positions[KNOTS_NUM - 1].1 );
+            self.visited.push(self.knots_positions[KNOTS_NUM - 1])
         }
     }
 
-    fn tail_recover_horizontal(&mut self) {
-        match self.tail_pos_x.cmp(&self.head_pos_x) {
-            Ordering::Greater => self.tail_pos_x -= 1,
-            Ordering::Less => self.tail_pos_x += 1,
+    fn tail_recover_horizontal(&mut self, knot_idx: usize) {
+        match self.knots_positions[knot_idx].0.cmp(&self.knots_positions[knot_idx - 1].0) {
+            Ordering::Greater => self.knots_positions[knot_idx].0 -= 1,
+            Ordering::Less => self.knots_positions[knot_idx].0 += 1,
             Ordering::Equal => (),
         }
     }
 
-    fn tail_recover_vertical(&mut self) {
-        match self.tail_pos_y.cmp(&self.head_pos_y) {
-            Ordering::Greater => self.tail_pos_y -= 1,
-            Ordering::Less => self.tail_pos_y += 1,
+    fn tail_recover_vertical(&mut self, knot_idx: usize) {
+        match self.knots_positions[knot_idx].1.cmp(&self.knots_positions[knot_idx - 1].1) {
+            Ordering::Greater => self.knots_positions[knot_idx].1 -= 1,
+            Ordering::Less => self.knots_positions[knot_idx].1 += 1,
             Ordering::Equal => (),
         }
     }
 
-    fn too_far(&self) -> bool {
-        self.head_pos_x.abs_diff(self.tail_pos_x) > 1
-            || self.head_pos_y.abs_diff(self.tail_pos_y) > 1
+    fn too_far_from_previous(&self, knot_idx: usize) -> bool {
+        self.knots_positions[knot_idx].0.abs_diff(self.knots_positions[knot_idx - 1].0) > 1
+            || self.knots_positions[knot_idx].1.abs_diff(self.knots_positions[knot_idx - 1].1) > 1
+    }
+}
+
+struct GraphicGrid {
+    visited: Coords,
+    hor_offset: usize,
+    ver_offset: usize,
+}
+
+impl GraphicGrid {
+    fn new(grid: &Grid) -> Self {
+        let mut min_x: usize = CENTER;
+        let mut max_x: usize = CENTER;
+        let mut min_y: usize = CENTER;
+        let mut max_y: usize = CENTER;
+
+        for (x, y) in grid.visited.iter() {
+            min_x = (*x).min(min_x);
+            min_y = (*y).min(min_y);
+            max_x = (*x).max(max_x);
+            max_y = (*y).max(max_y);
+        }
+
+        let visited_rescaled = grid.visited.iter().map(|(x,y)| (x - min_x, y - min_y)).collect();
+
+        Self { visited: visited_rescaled, hor_offset: max_x - min_x, ver_offset: max_y - min_y }
+    }
+
+    pub fn draw(&self) {
+        println!("ver_offset: {}, hor_offset: {}", self.ver_offset, self.hor_offset);
+
+        for y in 0..=self.ver_offset {
+            for x in 0..=self.hor_offset {
+                print!("{}", if self.visited.contains(&(x,self.ver_offset-y)) { "#" } else { "." });
+            }
+            println!("{}", "");
+        }
     }
 }
 
@@ -119,6 +157,8 @@ fn main() {
         for line in lines {
             row = line.unwrap();
 
+            println!("{}", row);
+
             if let Some((dir, n)) = row
                 .split_once(' ')
                 .map(|(dir, n)| (dir, n.parse::<usize>().unwrap()))
@@ -131,7 +171,10 @@ fn main() {
                     _ => (),
                 };
             }
+            let gg = GraphicGrid::new(&grid);
+            gg.draw();
         }
+
         println!("result: {acc}");
     }
 }
